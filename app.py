b6c3169f58fba5d94ba892c04ba03bd65ea03007
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-07-16 05:43:50 -1000

    cleaning up code

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def receive_text():
 	for i in range(0, 3):
 	    results += " " + response_dict['rows'][i]['bizname'] + " @ " 
 	    results += response_dict['rows'][i]['address'] + ";"
-	# 	    print 'Phone number: ' + str(response_dict['rows'][i]['phone'])
+	# 	print 'Phone number: ' + str(response_dict['rows'][i]['phone'])
 	resp = twilio.twiml.Response()
 	resp.message("Ask for 'free internet' at these places:" + results)
 	return str(resp)
