1204712237bbe8971369b01bae87ebc178586e59
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-08-04 06:30:49 -0700

    Fixed error

--- app.py
+++ app.py
@@ -59,9 +59,11 @@ def receive_text():
 	geo_lat = str(response_dict['rows'][0]['stop_lat'])
 	geo_long = str(response_dict['rows'][0]['stop_lon'])
 	lat_long = [geo_lat, geo_long]
-
+    
+    day = 'day' + arrow.now('US/Pacific').weekday()
+    
 	free_net_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT'
-	free_net_url += ' bizname, address, zip, phone, '
+	free_net_url += ' bizname, address, phone, ' + day + ','
 	free_net_url += 'ST_Distance(the_geom::geography, ST_PointFromText('
 	free_net_url += '\'POINT('+ geo_long + ' ' + geo_lat + ')\', 4326)'
 	free_net_url += '::geography) AS distance FROM freeweb ORDER BY distance '
@@ -72,8 +74,9 @@ def receive_text():
 
 	for i in range(0, 3):
 	    results += " " + response_dict['rows'][i]['bizname'] + " @ "
-	    results += response_dict['rows'][i]['address'] + ";"
-	# 	print 'Phone number: ' + str(response_dict['rows'][i]['phone'])
+	    results += response_dict['rows'][i]['address'] + ","
+		results += response_dict['rows'][i]['phone'] + " "
+		results += "hrs: " + response_dict['rows'][i][day]
 	resp = twilio.twiml.Response()
 	resp.message("Ask for 'free internet' at these places:" + results)
 	return str(resp)
