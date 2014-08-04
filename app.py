515e7c555736b4f64ad1339bdf04a7bb817800cf
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-08-04 06:13:40 -0700

    Fixed error

--- app.py
+++ app.py
@@ -45,19 +45,17 @@ def receive_text():
 		get_geo_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT '
 		get_geo_url += 'stop_lat, stop_lon FROM stops WHERE stop_id = '
 		get_geo_url += stop_ID[0]
+		response = urllib.urlopen(get_geo_url)
+		for line in response:
+			response_dict = simplejson.loads(line)	
+		if response_dict['total_rows'] == 0:
+			resp = twilio.twiml.Response()
+			resp.message(error_message)
+			return str(resp)
 	else:
-		error = True
-	response = urllib.urlopen(get_geo_url)
-	for line in response:
-		response_dict = simplejson.loads(line)
-		
-	if response_dict['total_rows'] == 0:
-		error = True
-	if error is True:
 		resp = twilio.twiml.Response()
 		resp.message(error_message)
-		return str(resp)
-		
+		return str(resp)	
 	geo_lat = str(response_dict['rows'][0]['stop_lat'])
 	geo_long = str(response_dict['rows'][0]['stop_lon'])
 	lat_long = [geo_lat, geo_long]
