189b333f1ebddf360614f5663a8cce232a57847a
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-08-04 04:59:26 -0700

    Fixed error

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def receive_text():
 		get_geo_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT '
 		get_geo_url += 'stop_lat, stop_lon FROM stops WHERE stop_id = '
 		get_geo_url += stop_ID[0]
-	else if len(stop_ID) == 0:
+	else:
 		error = True
 	response = urllib.urlopen(get_geo_url)
 	for line in response:
