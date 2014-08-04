1c0eaa48f3337e35796b474a2fa591a87248e187
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-08-04 04:30:09 -0700

    Parsed Stop ID from text

--- app.py
+++ app.py
@@ -3,6 +3,21 @@
 from flask import Flask, request, redirect
 import urllib, simplejson
 import twilio.twiml
+import gdata.spreadsheet.service
+import gdata.service
+import atom.service
+import gdata.spreadsheet
+import atom
+import arrow
+import re
+
+
+#Global Variables
+#gdata variables
+email_address = 'sfbrigade@gmail.com'
+password = 'hack4change'
+spreadsheet_key = '1S4jHX9__Drog_qqGsDJYFuO7KvRP9BUD8A95xQ5kkQU'
+worksheet_ID = 'od6'
 
 error_message = "We apologize for the inconvenience, we are unable to "
 error_message += "determine the closest 'free internet'. "
@@ -11,6 +26,8 @@ error_message += "Please try another Stop ID. Thank you!"
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
+
+
 def receive_text():
 	"""Function that performs main functionality of app ie;
 	logs received text message information to a Google Spreadsheet and replys
@@ -18,23 +35,27 @@ def receive_text():
 	locations for 'free internet' from the bus stop that corresponds to the
 	recieved ID.
 	"""
+	error = False
 	results = ""
-	stop_ID = request.values.get("Body")
+	stop_ID = re.findall('\d+', request.values.get("Body"))
 	phone_number = request.values.get("From")
-	#log_text_message(stop_ID, phone_number)
-
-	get_geo_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT '
-	get_geo_url += 'stop_lat, stop_lon FROM stops WHERE stop_id = '
-	get_geo_url += stop_ID
+	
+	if len(stop_ID) == 0:
+		error = True
+	else:	
+		log_text_message(stop_ID[0], phone_number)
+		get_geo_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT '
+		get_geo_url += 'stop_lat, stop_lon FROM stops WHERE stop_id = '
+		get_geo_url += stop_ID[0]
 	response = urllib.urlopen(get_geo_url)
 	for line in response:
 		response_dict = simplejson.loads(line)
-
-	if response_dict['total_rows'] == 0:
+		
+	if response_dict['total_rows'] == 0 or error:
 		resp = twilio.twiml.Response()
 		resp.message(error_message)
 		return str(resp)
-
+		
 	geo_lat = str(response_dict['rows'][0]['stop_lat'])
 	geo_long = str(response_dict['rows'][0]['stop_lon'])
 	lat_long = [geo_lat, geo_long]
@@ -58,6 +79,35 @@ def receive_text():
 	return str(resp)
 
 
+def log_text_message(stop_ID, phone_number):	
+	"""Function that logs the incoming text sender's phone number & the stop ID
+	into a Google Spreadsheet.
+	In args:           stop_ID, phone_number
+	Global vars in:    email_address, password, spreadsheet_key, worksheet_ID
+	"""
+	spr_client = gdata.spreadsheet.service.SpreadsheetsService()
+	spr_client.email = email_address
+	spr_client.password = password
+	spr_client.source = 'LocalFreeWeb text message app'
+	spr_client.ProgrammaticLogin()
+	entry = spr_client.InsertRow(build_data_dict(stop_ID, phone_number),
+	                             spreadsheet_key, worksheet_ID)
+
+
+def build_data_dict(stop_ID, phone_number):
+    """Function that builds a dictionary that includes the date, time,
+    phone_number and stop_ID of received text message
+    In args:    stop_ID, phone_number
+    Out arg:    dict
+    """
+    dict = {}
+    dict['date'] = arrow.now('US/Pacific').format('MM/DD/YYYY')
+    dict['time'] = arrow.now('US/Pacific').format('hh:mm:ss A')
+    dict['phone'] = phone_number
+    dict['stop'] = stop_ID
+    return dict
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
