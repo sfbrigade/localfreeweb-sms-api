1380687274f37d58aac99103de2c2e130bf4c427
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-09-23 11:54:01 -0700

    testing

--- app.py
+++ app.py
@@ -154,20 +154,13 @@ def generate_response_text(internet_resp_dict):
 #        results += str(internet_resp_dict['rows'][i]['address']) + " "
 #        results += str(internet_resp_dict['rows'][i]['phone']) + " | today's hrs: "
 #        results += str(internet_resp_dict['rows'][i][day]).strip() + ";"
-    results += " " + str(internet_resp_dict['rows'][0]['bizname']) + " "
-    results += str(internet_resp_dict['rows'][0]['address']) + " "
-    results += str(internet_resp_dict['rows'][0]['phone']) + " | today's hrs: "
-    results += str(internet_resp_dict['rows'][0][day]).strip() + ";"
-    
-    results += " " + str(internet_resp_dict['rows'][1]['bizname']) + " "
-    results += str(internet_resp_dict['rows'][1]['address']) + " "
-    results += str(internet_resp_dict['rows'][1]['phone']) + " | today's hrs: "
-    results += str(internet_resp_dict['rows'][1][day]).strip() + ";"
-    
-    results += " " + str(internet_resp_dict['rows'][2]['bizname']) + " "
-    results += str(internet_resp_dict['rows'][2]['address']) + " "
-    results += str(internet_resp_dict['rows'][2]['phone']) + " | today's hrs: "
-    results += str(internet_resp_dict['rows'][2][day]).strip() + ";"
+        
+    for i in internet_resp_dict['rows']:
+        results += " " + str(i['bizname']) + " "
+        results += str(i['address']) + " "
+        results += str(i['phone']) + " | today's hrs: "
+        results += str(i[day]).strip() + ";"        
+
     results = "Ask for 'free internet' at these places:" + results
     return generate_text_message(results)
 
